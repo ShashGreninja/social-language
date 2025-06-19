e06911b707a52c0895f5ba87ed45df68ec5eb30f
Shash <163015341+ShashGreninja@users.noreply.github.com>
2025-06-19 21:19:06 +0530

    Navbar is done!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
 import Image from "next/image";
 import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
+
 
 export default function Home() {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button className = "m-4">
-            Sign In
-          </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant={"secondary"}>Click Me</Button>
+    <div className = "m-4">
+      Home page content
     </div>
   );
 }
